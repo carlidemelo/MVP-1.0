af4c798d5e0b011e75bd2c69bb632d6b7c309c8c
Pablo <pablo.cqa1997@gmail.com>
2025-08-05 23:12:49 -0300

    Subida MVP 1.0

---
+++ challenge-amigo-secreto_pt-main/challenge-amigo-secreto_pt-main/app.js
@@ -0,0 +1,59 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+// Lista onde os nomes serão armazenados
+let listaDeAmigos = [];
+
+// Adiciona nome à lista
+function adicionarAmigo() {
+    const input = document.getElementById("amigo");
+    const nome = input.value.trim();
+
+    if (nome === "") {
+        alert("Digite um nome válido!");
+        return;
+    }
+
+    if (listaDeAmigos.includes(nome)) {
+        alert("Esse nome já foi adicionado.");
+        input.value = "";
+        return;
+    }
+
+    listaDeAmigos.push(nome);
+    atualizarLista();
+    input.value = "";
+}
+
+// Atualiza a lista na tela
+function atualizarLista() {
+    const ul = document.getElementById("listaAmigos");
+    ul.innerHTML = "";
+
+    listaDeAmigos.forEach(nome => {
+        const li = document.createElement("li");
+        li.textContent = nome;
+        ul.appendChild(li);
+    });
+}
+
+// Sorteia apenas um nome da lista
+function sortearAmigo() {
+    if (listaDeAmigos.length === 0) {
+        alert("Adicione ao menos um nome antes de sortear.");
+        return;
+    }
+
+    const sorteadoIndex = Math.floor(Math.random() * listaDeAmigos.length);
+    const nomeSorteado = listaDeAmigos[sorteadoIndex];
+
+    exibirResultado(`O(a) sorteado(a) foi: ${nomeSorteado}`);
+}
+
+// Mostra o nome sorteado na tela
+function exibirResultado(mensagem) {
+    const ulResultado = document.getElementById("resultado");
+    ulResultado.innerHTML = "";
+
+    const li = document.createElement("li");
+    li.textContent = mensagem;
+    ulResultado.appendChild(li);
+}
